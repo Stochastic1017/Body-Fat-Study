5a9310a45281e20c9c970c109d3c3bf4368eadc0
Shrivats S. <133294122+Stochastic1017@users.noreply.github.com>
2024-09-27 15:29:29 -0500

    created dashapp.py to visualize and explore data

--- dash/dashapp.py
+++ dash/dashapp.py
@@ -1,19 +1,38 @@
-from dash import Dash, html, dash_table
+from dash import Dash, dcc, html, dash_table
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
-# Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/Stochastic1017/Body_Fat_Study/refs/heads/main/dataset/BodyFat.csv')
 
-# Initialize the app
+df = pd.read_csv('https://raw.githubusercontent.com/Stochastic1017/Body_Fat_Study/refs/heads/main/dataset/BodyFat.csv')
 app = Dash()
 
-# App layout
+correlation_matrix = df.corr()
+
+heatmap_fig = go.Figure(
+    data=go.Heatmap(
+        z=correlation_matrix.values,
+        x=correlation_matrix.columns,
+        y=correlation_matrix.columns,
+        colorscale='Viridis'
+    )
+)
+heatmap_fig.update_layout(title='Correlation Heatmap')
+
 app.layout = [
-    html.Div(children='My First App with Data'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+    html.Div(children='Data Table'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=20)
 ]
 
-# Run the app
+app.layout = html.Div(children=[
+    html.H1(children='Correlation Heatmap Example'),
+
+    dcc.Graph(
+        id='correlation-heatmap',
+        figure=heatmap_fig
+    )
+])
+
+
 if __name__ == '__main__':
     app.run(debug=True)
