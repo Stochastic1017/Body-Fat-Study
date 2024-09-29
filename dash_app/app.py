a69a5ba566ce311b2f45a1b09cbf61b335549029
Shrivats S. <133294122+Stochastic1017@users.noreply.github.com>
2024-09-29 14:14:31 -0500

    Update app.py

--- dash_app/app.py
+++ dash_app/app.py
@@ -4,7 +4,9 @@ from landing_page.cover_page import layout as landing_layout
 from first_page.introduction_description import layout as first_layout
 from second_page.exploratory_data_visualization import layout as second_layout
 
-app = Dash(__name__, suppress_callback_exceptions=True, external_scripts=['https://cdn.plot.ly/plotly-latest.min.js'])
+app = Dash(__name__, 
+           suppress_callback_exceptions=True, 
+           external_scripts=['https://cdn.plot.ly/plotly-latest.min.js'])
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
